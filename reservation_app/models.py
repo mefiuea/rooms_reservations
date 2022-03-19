a8051dcf87255e3ed142c75df31eeb8a18d7b147
Maciej Koczot <mefiuea@gmail.com>
2022-03-19 11:46:28 +0100

    copy models from previous version of this project

--- reservation_app/models.py
+++ reservation_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+    size = models.PositiveSmallIntegerField()
+    projector_available = models.BooleanField(default=False)
+
+
+class ReservationStatus(models.Model):
+    date = models.DateField()
+    room = models.ForeignKey('Room', on_delete=models.CASCADE, related_name='reservations')  # 'room_id' in database
+    comment = models.TextField()
+
+    class Meta:
+        unique_together = ('date', 'room_id')
